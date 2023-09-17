f101d2ddf7aa6654296210265e91cdf346878f18
Tulenien <timofey.evsigneev@gmail.com>
2023-09-17 20:55:36 +0200

    feat: implement Right Triangle check

--- FiguresSquare/Triangle.cs
+++ FiguresSquare/Triangle.cs
@@ -2,16 +2,112 @@
 {
     public class Triangle : IFigure
     {
-        private double _sideA;
-        private double _sideB;
-        private double _sideC;
+        private double[] _sides = new double[3];
 
-        private double CalculateSemiPerimeter() => (_sideA + _sideB + _sideC) / 2;
+        public Triangle(double a, double b, double c)
+        {
+            _sides[0] = a;
+            _sides[1] = b;
+            _sides[2] = c;
+        }
+
+        private double CalculateSemiPerimeter(double a, double b, double c)
+        {
+            return (a + b + c) / 2;
+        }
+
+        private int GetIndexOfTheLongestSide()
+        {
+            double a = _sides[0];
+            double b = _sides[1];
+            double c = _sides[2];
+
+            if (a > b)
+            {
+                if (b > c)
+                {
+                    return 0;
+                }
+                else if (c > a)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if (c > b)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private bool CheckIfRight()
+        {
+            int longestSideIndex = GetIndexOfTheLongestSide();
+            double longest = 0, a = 0, b = 0;
+            switch (longestSideIndex)
+            {
+                case 0:
+                    longest = _sides[0];
+                    a = _sides[1];
+                    b = _sides[2];
+                    break;
+                case 1:
+                    longest = _sides[1];
+                    a = _sides[0];
+                    b = _sides[2];
+                    break;
+                case 2:
+                    longest = _sides[2];
+                    a = _sides[0];
+                    b = _sides[1];
+                    break;
+            }
+            return longest * longest == a * a + b * b;
+        }
+
+        private double CalculateAreaOfRightTriangle()
+        {
+            int longestSideIndex = GetIndexOfTheLongestSide();
+            double a = 0, b = 0;
+            switch (longestSideIndex)
+            {
+                case 0:
+                    a = _sides[1];
+                    b = _sides[2];
+                    break;
+                case 1:
+                    a = _sides[0];
+                    b = _sides[2];
+                    break;
+                case 2:
+                    a = _sides[0];
+                    b = _sides[1];
+                    break;
+            }
+            return 0.5 * a * b;
+        }
 
         public double CalculateArea()
         {
-            double semiPerimeter = CalculateSemiPerimeter();
-            return Math.Sqrt(semiPerimeter * (semiPerimeter - _sideA) * (semiPerimeter - _sideB) * (semiPerimeter - _sideC));
+            if (CheckIfRight() != true)
+            {
+                double a = _sides[0];
+                double b = _sides[1];
+                double c = _sides[2];
+                double semiPerimeter = CalculateSemiPerimeter(a, b, c);
+                return Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+            }
+            else
+            {
+                return CalculateAreaOfRightTriangle();
+            }
         }
     }
 }
