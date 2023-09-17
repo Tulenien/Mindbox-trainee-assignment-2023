87ac2d8c37f49db7c8999ead7d346e577355f44c
Tulenien <timofey.evsigneev@gmail.com>
2023-09-17 19:10:41 +0200

    feat: add naive implementation of area calculation

---
+++ FiguresSquare/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Circle : IFigure
+    {
+        private double _radius;
+        public double CalculateArea() => Math.PI * _radius * _radius;
+    }
+}
---
+++ FiguresSquare/Triangle.cs
@@ -0,0 +1,17 @@
+﻿namespace Figures
+{
+    public class Triangle : IFigure
+    {
+        private double _sideA;
+        private double _sideB;
+        private double _sideC;
+
+        private double CalculateSemiPerimeter() => (_sideA + _sideB + _sideC) / 2;
+
+        public double CalculateArea()
+        {
+            double semiPerimeter = CalculateSemiPerimeter();
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - _sideA) * (semiPerimeter - _sideB) * (semiPerimeter - _sideC));
+        }
+    }
+}
