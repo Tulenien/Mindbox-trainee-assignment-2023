4a588a93b485de19b15bacaf635bb177572a4271
Tulenien <timofey.evsigneev@gmail.com>
2023-09-17 21:24:47 +0200

    fix: rename folder, add constructor to Circle class

--- Figures/Circle.cs
+++ Figures/Circle.cs
@@ -9,6 +9,12 @@ namespace Figures
     public class Circle : IFigure
     {
         private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
         public double CalculateArea() => Math.PI * _radius * _radius;
     }
 }
